21b0495a70114a0580e7424d445fef88e194458b
Zylleon <56230821+Zylleon@users.noreply.github.com>
2026-07-12 09:18:51 -0600

    initial commit

---
+++ Source/ZWB_FaeForest/BiomeWorker_FaeForest.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld.Planet;
+using Verse;
+
+namespace ZWB_LotusWilds
+{
+    public class BiomeWorker_FaeForest : BiomeWorker
+    {
+
+        public override float GetScore(BiomeDef biome, Tile tile, PlanetTile planetTile)
+        {
+            if (tile.WaterCovered)
+            {
+                return -100f;
+            }
+
+            if(tile.rainfall < 1400f || tile.rainfall > 2600f)
+            {
+                return 0;
+            }
+            if(tile.temperature < -1f || tile.temperature > 11f)
+            {
+                return 0;
+            }
+            if(tile.swampiness > 0.5f)
+            {
+                return 0f;
+            }
+            if (tile.elevation < 300f)
+            {
+                return 0f;
+            }
+
+
+            return 16.1f + tile.temperature;
+        }
+    }
+
+
+}
